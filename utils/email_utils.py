3a74a78ead49d9be203b528cb8ffb5df098e1ff6
unknown <ffarzam_1992@yahoo.com>
2023-11-10 02:08:33 +0330

    chore: update email utils

--- utils/email_utils.py
+++ utils/email_utils.py
@@ -18,10 +18,21 @@ def get_reset_password_template(token):
             """
 
 
+def get_rss_update_notification_template(channel_name):
+    return f"""
+            <html>
+            <body>
+            <p>Hi !!! <br>{channel_name} has been updated</p>
+            </body>
+            </html>
+            """
+
+
 def template_mapper(arg, token):
 
     mapper = {
         "verify account": get_verify_template,
         "reset password": get_reset_password_template,
+        "rss_update_notification": get_rss_update_notification_template
     }
     return mapper[arg](token)
